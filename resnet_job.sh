853d80cf1fd304b934abce0dc2379227b32992e5
Jason Anderson <58935166+jand271@users.noreply.github.com>
2023-05-28 12:04:52 -0700

    std out 2

--- resnet_job.sh
+++ resnet_job.sh
@@ -12,7 +12,7 @@ module load cuda/11.0
 current_datetime=$(date +"%D %T")
 echo "Current date and time: $current_datetime"
 echo "Starting Script"
-stdbuf -oL python3 try_spoof_resnet.py
+python3 -u try_spoof_resnet.py
 
 echo "Ending Script"
 current_datetime=$(date +"%D %T")
