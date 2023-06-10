e77bc287d786bf2bce3073b0d960580fd76a004b
imaginary-person <sanchit@stanford.edu>
2023-06-10 12:59:33 -0700

    Add description to fgsm_regressor.py

--- fgsm_regressor.py
+++ fgsm_regressor.py
@@ -1,3 +1,45 @@
+################################################################################
+#                                                                              #
+# One approach we tried is to let the generator learn from the noises created  #
+# by FGSM. FGSM creates powerful and almost imperceptible adversarial noises.  #
+# Using a stepping method, we created noise for each image in the Oxford-IIIT  #
+# pets dataset, and trained the generator as a regressor of these pregenerated #
+# noises.                                                                      #
+#                                                                              #
+# The amount of change that the regressor noises make to the original image    #
+# appears to be correlated with the inaccuracy it generates for the spoofed    #
+# model. The generated images suggests that the regressor training framework's #
+# not practically useful to train the adversarial noise neural networks. The   #
+# noise that the resulting regressor generates create too much perturbation to #
+# the original image, so the change is clearly visible to human eyes.          #
+#                                                                              #
+# Authors: Cheng Chang <chc012@stanford.edu>                                   #
+#                                                                              #
+# Usage: python fgsm_regressor.py                                              #
+#                                                                              #
+#                                                                              #
+# Copyright 2023 Cheng Chang <chc012@stanford.edu>                             #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person obtaining a copy #
+# of this software and associated documentation files (the “Software”), to     #
+# deal in the Software without restriction, including without limitation the   #
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or  #
+# sell copies of the Software, and to permit persons to whom the Software is   #
+# furnished to do so, subject to the following conditions:                     #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included in   #
+# all copies or substantial portions of the Software.                          #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,              #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF           #
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO #
+# EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,        #
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR        #
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE    #
+# USE OR OTHER DEALINGS IN THE SOFTWARE.                                       #
+#                                                                              #
+################################################################################
+
 from timm.data.transforms_factory import create_transform
 from timm.data import resolve_data_config
 import torch
